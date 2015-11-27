8e622618e0678c499eb7a18c66df993305872ae3
Kenice N <kenicenoel@outlook.com>
2015-11-27 14:30:18 -0400

    Azure Web App Compatible

--- includes/config.php
+++ includes/config.php
@@ -7,12 +7,20 @@ $user = "admin";
 $pass = "admin";
 $database = "websource_package_data";
 
+
+
 // The production MySql server
 // $host = "websource-caribbean.com";
 // $user = "packagehero";
 // $pass = "PackageH3r0";
 // $database = "packagehero_db";
 
+
+// Connect to the Azure mySQL server
+$host = "br-cdbr-azure-south-a.cloudapp.net";
+$user = "b80dca2c05801a";
+$pass = "349729b5";
+$database = "DefaultMySQL";
 
 // Create connection
 $connection = new mysqli($host, $user, $pass, $database);
