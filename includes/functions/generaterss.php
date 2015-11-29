0629da438631c8d5bdec52fa87e7d90d3b5322c7
Kenice <kenicenoel@outlook.com>
2015-11-29 12:42:46 -0400

    RSS feed now works

--- includes/functions/generaterss.php
+++ includes/functions/generaterss.php
@@ -8,9 +8,7 @@ echo "<channel>";
 
 	   include_once ('../config.php');
 
-	    $sql = " SELECT PackageID, TrackingNumber, CustomerName, MainIssue, Description, IssueCreationTime FROM packages ORDER BY PackageID LIMIT 10";
-
-
+	    $sql = " SELECT PackageID, TrackingNumber, CustomerName, MainIssue, Description, Photo1, IssueCreationTime FROM packages ORDER BY IssueCreationTime DESC";
 		  // prepare the sql statement
 		  $stmt = $connection->prepare($sql);
 
@@ -21,22 +19,27 @@ echo "<channel>";
 		  $stmt->store_result();
 
 		  /* Bind the results to variables */
-		  $stmt->bind_result($packageid, $tnumber, $customername, $mainissue, $description, $issuedate);
+		  $stmt->bind_result($packageid, $tnumber, $customername, $mainissue, $description, $photo, $issuedate);
 
       while($stmt->fetch())
       {
 
 		  	$pubdate = date('D j F Y g:i A', strtotime($issuedate));
-			  $title = "A new issue has been created for a package with a Tracking Number ".$tnumber.".";
-			$desc = "Here are the details of the issue:<br>Main Issue: ".$mainissue;
-        	$items .=
+			  $title = "A new issue has been created for a package with Tracking Number ".$tnumber.".";
+			  $desc = "<header>Hello,</header><br>A new issue has been added to Package Hero.<br>Here are the details of the issue:";
+        $desc.= "<br><img src='../".$photo."' />";
+        $desc.= "<br>Tracking Number: ".$tnumber;
+        $desc.= "<br>Main Issue: ".$mainissue;
+        $desc.= "<br>Customer: ".$customername;
+
+        $items .=
 			'
 			<item>
 				<title>'.$title.'</title>
 				<link>'.$link.'</link>
-				<guid>'.$packageid.'</link>
+				<guid>'.$packageid.'</guid>
 				<pubDate>'.$pubdate.'</pubDate>
-				<description>'.$desc.'</description>
+				<description><![CDATA['.$desc.']]></description>
 			</item>';
       }
 
