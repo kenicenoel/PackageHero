54157e8e576b553a65716139b0424d224300c764
Kenice <kenicenoel@outlook.com>
2015-11-28 22:48:58 -0400

    Working RSS

--- includes/functions/generaterss.php
+++ includes/functions/generaterss.php
@@ -1,10 +1,13 @@
-<?php header('Content-Type: text/xml'); ?>
-<?xml version='1.0' encoding='UTF-8'?>
-<rss version="2.0">
-  <channel>
-    <?php
-	include_once ('../config.php');
-	
+<?php
+    header("Content-Type: application/rss+xml; charset=ISO-8859-1");
+
+
+echo '<?xml version="1.0" encoding="ISO-8859-1"?>';
+echo "<rss version='2.0'>";
+echo "<channel>";
+
+	   include_once ('../config.php');
+
 	    $sql = " SELECT PackageID, TrackingNumber, CustomerName, MainIssue, Description, IssueCreationTime FROM packages ORDER BY PackageID LIMIT 10";
 
 
@@ -22,12 +25,10 @@
 
       while($stmt->fetch())
       {
-		  	
+
 		  	$pubdate = date('D j F Y g:i A', strtotime($issuedate));
-			$title = "A new issue has been created for a package with a Tracking Number ".$tnumber.".";
-			$desc = 
-			"Here are the details of the issue:<br>
-			Main Issue: ".$mainissue."
+			  $title = "A new issue has been created for a package with a Tracking Number ".$tnumber.".";
+			$desc = "Here are the details of the issue:<br>Main Issue: ".$mainissue;
         	$items .=
 			'
 			<item>
@@ -35,51 +36,18 @@
 				<link>'.$link.'</link>
 				<guid>'.$packageid.'</link>
 				<pubDate>'.$pubdate.'</pubDate>
-				<description>'.$description.'</description>
-				<
-
-
-
-          
-
+				<description>'.$desc.'</description>
+			</item>';
       }
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
     	echo '
 		<title>Package Hero Updates</title>
 		<link>http://packagehero.websource-caribbean.com</link>
-		<description>Receive updates on Package Hero Issues</description>
-		
-	
-	
-	
-	
-	
-	
-	
-	
-	
-    ?>
-  </channel>
-</rss>
+		<description>Receive updates on Package Hero Issues</description>';
+    echo $items;
+
+
+echo "</channel>";
+echo "</rss>";
+
+  ?>
