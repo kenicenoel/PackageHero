2319b25da2f207743e27794e159473f134e58048
Kenice N <kenicenoel@outlook.com>
2015-12-02 14:07:10 -0400

    Fixed search bug
    Fixed  a bug where if a user searched for nothing, 2 search results were
    returned regardless.

--- includes/packagelookup.php
+++ includes/packagelookup.php
@@ -1,27 +1,35 @@
 <?php
 include_once("config.php");
 
+// if either a post or get request is received, check if query is set which means someone wants to search
 if(isset($_POST['query']) || isset($_GET['query']) )
 {
-
+      // if a post request was received check if there is a value and if so, make $query that value
       if(isset($_POST['query']) && ($_POST['query'] !=''))
       {
         $query = "%{$_POST['query']}%";
       }
 
+      // if a get request was received check if there is a value and if so, make $query that value
       else if(isset($_GET['query']) && ($_GET['query'] !=''))
       {
         $query = "%{$_GET['query']}%";
       }
-
-      else
-      {
-        $query='';
-      }
+      //
+      // else
+      // {
+      //   $query='';
+      // }
         $before = "";
         $after = "";
-        if(isset($_POST['beforeDate'])) { $before = $_POST['beforeDate']; }
-        if(isset($_POST['afterDate'])) { $after = $_POST['afterDate']; }
+        if(isset($_POST['beforeDate']))
+        {
+          $before = $_POST['beforeDate'];
+        }
+        if(isset($_POST['afterDate']))
+        {
+          $after = $_POST['afterDate'];
+        }
         $sql = "SELECT TrackingNumber, AccountNumber, CustomerName, MainIssue, Resolved, Description, Photo1, ItemType, ShippingCarrier FROM packages WHERE (packages.IssueCreationTime BETWEEN ? AND ?)  OR packages.TrackingNumber Like ? OR packages.CustomerName Like ? OR packages.ItemType LIKE ? OR packages.ShippingCarrier LIKE ?";
 
         // echo $before;
